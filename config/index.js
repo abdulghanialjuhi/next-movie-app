45e52261d58d3a649ee37d7237d435a3ce28946c
abdulghanialjuhi <gfyjd@hotmail.com>
2022-07-14 21:56:04 +0800

    skleton

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production'
 
 export const nextApi = dev ? 'http://localhost:3000' : 'https://next-movie-iz91wfaaq-abdulghanialjuhi.vercel.app'
 
-export const server = dev ? 'http://127.0.0.1:5000' : 'https://next-movie-5kpanjoqg-abdulghanialjuhi.vercel.app'
+export const server = dev ? 'http://127.0.0.1:5000' : 'https://next-movie-app-abdulghanialjuhi.vercel.app'
