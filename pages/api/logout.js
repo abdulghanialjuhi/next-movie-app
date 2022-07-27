567d2c5787eb48e48f105ea0da7d31abaaccbbf7
abdulghanialjuhi <gfyjd@hotmail.com>
2022-07-27 17:37:59 +0800

    up

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -3,14 +3,7 @@ import cookie from 'cookie'
 export default async function logOut(req, res) {
 
   try {
-    res.setHeader('set-cookie', 
-    cookie.serialize('access_token_cookie', '', {
-        httpOnly: true,
-        secure: true,
-        expires: new Date(0),
-        sameSite: 'none',
-        path: '/'
-    }))
+    res.setHeader('Set-Cookie', 'access_token_cookie=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT');
     res.status(200).json({msg: 'Logged out successfully'})
   } catch (err) {
     res.status(400).json({err: 'Error, Please try again'})
